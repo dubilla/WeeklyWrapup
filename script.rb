9f4e3c206b0714232bcfff7d0b2fe40dbc08dc9a
Dan Ubilla <dan.ubilla@gmail.com>
2016-02-10 22:38:07 -0500

    Add request for opponents

--- script.rb
+++ script.rb
@@ -1,3 +1,15 @@
 #!/usr/bin/env ruby
+require 'httparty'
+require 'nokogiri'
+require 'open-uri'
 
+leagueID = ENV['LEAGUE_ID']
+leagueUrl = "http://games.espn.go.com/fba/scoreboard?leagueId=#{leagueID}"
 
+doc = Nokogiri::HTML(open(leagueUrl))
+puts leagueUrl
+matchupContainer = doc.css('#scoreboardMatchups')
+matchups = matchupContainer.css('.matchup')
+opponents = matchups.map{|m| m.css('.name').map{|o| o.text } }
+
+puts opponents
