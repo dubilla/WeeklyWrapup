39bfa30d0090f33b4a9e90fdd9b5edeb1e0cb1d6
Dan Ubilla <dan.ubilla@gmail.com>
2016-02-15 21:06:55 -0500

    Add parser for matchup opponents and scores

--- script.rb
+++ script.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+
 require 'httparty'
 require 'nokogiri'
 require 'open-uri'
@@ -7,9 +8,8 @@ leagueID = ENV['LEAGUE_ID']
 leagueUrl = "http://games.espn.go.com/fba/scoreboard?leagueId=#{leagueID}"
 
 doc = Nokogiri::HTML(open(leagueUrl))
-puts leagueUrl
 matchupContainer = doc.css('#scoreboardMatchups')
 matchups = matchupContainer.css('.matchup')
-opponents = matchups.map{|m| m.css('.name').map{|o| o.text } }
+matchupsDetails = matchups.map{|m| { name: m.css('.name').map{|o| o.text }, score: m.css('.score').map{|o| o.text } } }
 
-puts opponents
+puts matchupsDetails
