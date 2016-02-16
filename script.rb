d540e99eaf5372ba5241bc58ed882c87244be99a
Dan Ubilla <dan.ubilla@gmail.com>
2016-02-15 21:40:25 -0500

    Reorganize matchups data

--- script.rb
+++ script.rb
@@ -3,13 +3,21 @@
 require 'httparty'
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 leagueID = ENV['LEAGUE_ID']
 leagueUrl = "http://games.espn.go.com/fba/scoreboard?leagueId=#{leagueID}"
 
 doc = Nokogiri::HTML(open(leagueUrl))
-matchupContainer = doc.css('#scoreboardMatchups')
-matchups = matchupContainer.css('.matchup')
-matchupsDetails = matchups.map{|m| { name: m.css('.name').map{|o| o.text }, score: m.css('.score').map{|o| o.text } } }
+matchupContainerHtml = doc.css('#scoreboardMatchups')
+matchupsHtml = matchupContainerHtml.css('.matchup')
+matchupsDetails = matchupsHtml.map{|m| { name: m.css('.name').map{|o| o.text }, score: m.css('.score').map{|o| o.text } } }
+matchups = []
+matchupsDetails.each do |m|
+  matchups.push({
+    awayOpponent: { name: m[:name].first, score: m[:score].first },
+    homeOpponent: { name: m[:name].last, socre: m[:score].last }
+  })
+end
 
-puts matchupsDetails
+puts matchups
